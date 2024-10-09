aa409bf60ad5ef0b4261aafde10b14ddb7340d14
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-10-09 19:34:57 +0300

    ui improve and connected websites

--- sakuragram/Views/Settings/PrivacyAndSecurity.xaml.cs
+++ sakuragram/Views/Settings/PrivacyAndSecurity.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Threading.Tasks;
 using CommunityToolkit.WinUI;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using sakuragram.Services;
 using sakuragram.Views.Settings.AdditionalElements;
 using TdLib;
 
@@ -10,7 +12,11 @@ namespace sakuragram.Views.Settings;
 
 public partial class PrivacyAndSecurity : Page
 {
-    private TdClient _client = App._client;
+    private readonly TdClient _client = App._client;
+    
+    private bool _canOpenBlockedUsers = false;
+    private bool _canOpenConnectedWebsites = false;
+    private bool _canOpenActiveSessions = false;
     
     public PrivacyAndSecurity()
     {
@@ -28,8 +34,11 @@ public partial class PrivacyAndSecurity : Page
         var activeSessions = await _client.ExecuteAsync(new TdApi.GetActiveSessions());
         
         CardBlockedUsers.Description = $"There are currently {blockedUsers.TotalCount} blocked users";
+        _canOpenBlockedUsers = true;
         CardConnectedWebsites.Description = $"There are currently {connectedWebsites.Websites.Length} connected websites";
+        _canOpenConnectedWebsites = true;
         CardActiveSessions.Description = $"There are currently {activeSessions.Sessions_.Length} active sessions";
+        _canOpenActiveSessions = true;
     }
     
     private void DeleteIfAway_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -60,7 +69,7 @@ public partial class PrivacyAndSecurity : Page
         }
     }
     
-    private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    private async void ButtonSessions_OnClick(object sender, RoutedEventArgs e)
     {
         await ContentDialogSessions.ShowAsync();
     }
@@ -81,5 +90,99 @@ public partial class PrivacyAndSecurity : Page
     {
         ActiveSession.Children.Clear();
         SessionList.Children.Clear();
+    }
+
+    private async void ContentDialogWebsites_OnOpened(ContentDialog sender, ContentDialogOpenedEventArgs args)
+    {
+        var websites = await _client.ExecuteAsync(new TdApi.GetConnectedWebsites()).ConfigureAwait(false);
+
+        await DispatcherQueue.EnqueueAsync(() =>
+        {
+            foreach (var website in websites.Websites)
+            {
+                GenerateWebsiteEntry(website);
+            }
+        });
+    }
+
+    private async void GenerateWebsiteEntry(TdApi.ConnectedWebsite website)
+    {
+        TdApi.User bot = await _client.GetUserAsync(website.BotUserId);
+        
+        Border border = new();
+        border.HorizontalAlignment = HorizontalAlignment.Stretch;
+        border.VerticalAlignment = VerticalAlignment.Stretch;
+        
+        StackPanel mainStackPanel = new();
+        mainStackPanel.Orientation = Orientation.Horizontal;
+        mainStackPanel.HorizontalAlignment = HorizontalAlignment.Stretch;
+        mainStackPanel.VerticalAlignment = VerticalAlignment.Stretch;
+        
+        StackPanel textStackPanel = new();
+        textStackPanel.Orientation = Orientation.Vertical;
+        textStackPanel.HorizontalAlignment = HorizontalAlignment.Stretch;
+        textStackPanel.VerticalAlignment = VerticalAlignment.Stretch;
+        
+        PersonPicture botPhoto  = new();
+        botPhoto.Margin = new Thickness(10);
+        botPhoto.Width = 42;
+        botPhoto.Height = 42;
+        mainStackPanel.Children.Add(botPhoto);
+        MediaService.GetUserPhoto(bot, botPhoto);
+        
+        TextBlock botNameTextBlock = new();
+        botNameTextBlock.FontSize = 14;
+        botNameTextBlock.Text = bot.FirstName + " " + bot.LastName;
+        textStackPanel.Children.Add(botNameTextBlock);
+        
+        TextBlock websiteInfo = new();
+        websiteInfo.FontSize = 12;
+        websiteInfo.Text = $"{website.DomainName}, {website.Browser}, {website.Platform}";
+        textStackPanel.Children.Add(websiteInfo);
+        
+        TextBlock websiteLocation = new();
+        websiteLocation.FontSize = 12;
+        websiteLocation.Foreground = new SolidColorBrush(Colors.Gray);
+        websiteLocation.Text = website.Location + ", " + MathService.CalculateDateTime(website.LogInDate).ToShortDateString();
+        textStackPanel.Children.Add(websiteLocation);
+
+        Button disconnectButton = new();
+        disconnectButton.Content = new FontIcon() { Glyph = "\uE8A3" };
+        disconnectButton.Click += (sender, args) => DisconnectButton_Click(sender, args, website.Id);
+        disconnectButton.HorizontalAlignment = HorizontalAlignment.Right;
+        disconnectButton.VerticalAlignment = VerticalAlignment.Center;
+        
+        mainStackPanel.Children.Add(textStackPanel);
+        mainStackPanel.Children.Add(disconnectButton);
+        border.Child = mainStackPanel;
+        PanelWebsites.Children.Add(border);
+    }
+
+    private async void DisconnectButton_Click(object sender, RoutedEventArgs e, long websiteId)
+    {
+        await _client.DisconnectWebsiteAsync(websiteId);
+        PanelWebsites.Children.Clear();
+        ContentDialogWebsites_OnOpened(null, null);
+    }
+
+    private async void ButtonDisconnectAllWebsites_OnClick(object sender, RoutedEventArgs e)
+    {
+        await _client.DisconnectAllWebsitesAsync();
+        PanelWebsites.Children.Clear();
+        ContentDialogWebsites_OnOpened(null, null);
+    }
+    
+    private void ContentDialogWebsites_OnClosed(ContentDialog sender, ContentDialogClosedEventArgs args)
+    {
+        PanelWebsites.Children.Clear();
+    }
+
+    private void ButtonViewWebsites_OnClick(object sender, RoutedEventArgs e)
+    {
+        ContentDialogWebsites.ShowAsync();
+    }
+
+    private void ButtonBlockedUsers_OnClick(object sender, RoutedEventArgs e)
+    {
     }
 }
