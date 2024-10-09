92c27a10c7c59405c6a71c2e0c5875ff5329782c
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-10-09 18:36:17 +0300

    Create MathService.cs

---
+++ sakuragram/Services/MathService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace sakuragram.Services;
+
+public class MathService
+{
+    public static DateTime CalculateDateTime(int time)
+    {
+        try
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            dateTime = dateTime.AddSeconds(time).ToLocalTime();
+
+            return dateTime;
+        } 
+        catch (Exception e)
+        {
+            Debug.WriteLine(e);
+            return DateTime.MinValue;
+        }
+    }
+}
