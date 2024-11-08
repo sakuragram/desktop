a014238869e32ac541e36e3ca08467c7b3d093c6
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-11-08 22:20:02 +0300

    global changes in main window😮

--- sakuragram/MainWindow.xaml.cs
+++ sakuragram/MainWindow.xaml.cs
@@ -4,15 +4,16 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.UI;
+using ABI.Microsoft.UI;
+using CommunityToolkit.WinUI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using Octokit;
-using sakuragram.Services;
-using sakuragram.Views;
+using sakuragram.Services.Core;
 using TdLib;
-using Application = ABI.Microsoft.UI.Xaml.Application;
 using DispatcherQueuePriority = Microsoft.UI.Dispatching.DispatcherQueuePriority;
 
 namespace sakuragram
@@ -65,57 +66,16 @@ namespace sakuragram
 			#endif
 			
             ExtendsContentIntoTitleBar = true;
-            NavigationView.SelectedItem = NavigationView.MenuItems[0];
-            NavigateToView("ChatsView", null);
+            AppWindow.TitleBar.ButtonBackgroundColor = Microsoft.UI.Colors.Transparent;
             TrySetDesktopAcrylicBackdrop();
-            
-            var chatsIds = _client.ExecuteAsync(new TdApi.GetChats{Limit = 100}).Result.ChatIds;
-            foreach (var chatId in chatsIds)
-            {
-	            var chat = _client.ExecuteAsync(new TdApi.GetChat {ChatId = chatId}).Result;
-	            _totalUnreadCount += chat.UnreadCount;
-            }
-            
-			UnreadMessagesCount.Value = _totalUnreadCount;
-			
-			_user = _client.GetMeAsync().Result;
-			NavigationView.PaneTitle = $"{_user.FirstName} ({_totalUnreadCount})";
-			
-			foreach (var chatFolderInfo in _folders)
-			{
-				try
-				{
-					string path = AppContext.BaseDirectory +
-					              $@"Assets\icons\folders\folders_{chatFolderInfo.Icon.Name.ToLower()}.png";
-					BitmapIcon folderIcon = new();
-					
-					if (File.Exists(path))
-					{
-						Uri folderIconPath = new(path);
-						
-						folderIcon.UriSource = folderIconPath;
-						folderIcon.ShowAsMonochrome = false;
-					}
-			
-					var folderItem = new NavigationViewItem();
-					//folderItem.Icon = folderIcon != null ? folderIcon : null;
-					folderItem.Content = chatFolderInfo.Title;
-					folderItem.Tag = "ChatsView";
-					folderItem.Name = $"{chatFolderInfo.Title}_{chatFolderInfo.Id}";
-			
-					NavigationView.MenuItems.Add(folderItem);
-				}
-				catch (UriFormatException e)
-				{
-					Debug.WriteLine($"Error: {e.Message})");
-					throw;
-				}
-			}
 
 			CheckForUpdates();
 			
             _client.UpdateReceived += async (_, update) => { await ProcessUpdates(update); };
             NotificationService notificationService = new();
+
+            var chatsView = Assembly.GetExecutingAssembly().GetType($"{Config.AppName}.Views.ChatsView");
+            ContentFrame.Navigate(chatsView);
 		}
 
 		private async void CheckForUpdates()
@@ -140,25 +100,23 @@ namespace sakuragram
 				case TdApi.Update.UpdateNewMessage updateNewMessage:
 				{
 					_totalUnreadCount += 1;
-					NavigationView.DispatcherQueue.TryEnqueue(DispatcherQueuePriority.High,
-						() => NavigationView.PaneTitle = $"{_user.FirstName} ({_totalUnreadCount})");
-					UnreadMessagesCount.DispatcherQueue.TryEnqueue(DispatcherQueuePriority.High,
-						() => UnreadMessagesCount.Value = _totalUnreadCount);
+					//DispatcherQueue.EnqueueAsync(() => NavigationView.PaneTitle = $"{_user.FirstName} ({_totalUnreadCount})");
+					//DispatcherQueue.EnqueueAsync(() => UnreadMessagesCount.Value = _totalUnreadCount);
 					break;
 				}
 				case TdApi.Update.UpdateConnectionState updateConnectionState:
 				{
 					DispatcherQueue.TryEnqueue(DispatcherQueuePriority.Normal, () =>
 					{
-						NavigationView.PaneTitle = updateConnectionState.State switch
-						{
-							TdApi.ConnectionState.ConnectionStateReady => $"{_user.FirstName} ({_totalUnreadCount})",
-							TdApi.ConnectionState.ConnectionStateUpdating => "Updating...",
-							TdApi.ConnectionState.ConnectionStateConnecting => "Connecting...",
-							TdApi.ConnectionState.ConnectionStateWaitingForNetwork => "Waiting for network...",
-							TdApi.ConnectionState.ConnectionStateConnectingToProxy => "Connecting to proxy...",
-							_ => Config.AppName
-						};
+						// NavigationView.PaneTitle = updateConnectionState.State switch
+						// {
+						// 	TdApi.ConnectionState.ConnectionStateReady => $"{_user.FirstName} ({_totalUnreadCount})",
+						// 	TdApi.ConnectionState.ConnectionStateUpdating => "Updating...",
+						// 	TdApi.ConnectionState.ConnectionStateConnecting => "Connecting...",
+						// 	TdApi.ConnectionState.ConnectionStateWaitingForNetwork => "Waiting for network...",
+						// 	TdApi.ConnectionState.ConnectionStateConnectingToProxy => "Connecting to proxy...",
+						// 	_ => Config.AppName
+						// };
 					});
 					break;
 				}
@@ -185,82 +143,9 @@ namespace sakuragram
 
 		}
 
-		private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
-		{
-			var item = args.InvokedItemContainer as NavigationViewItem;
-			if (item == null || item == _lastItem)
-				return;
-
-			var clickedView = item.Tag.ToString();
-
-			if (!NavigateToView(clickedView, item)) return;
-			_lastItem = item;
-		}
-
-		private bool NavigateToView(string clickedView, NavigationViewItem item)
-		{
-			switch (clickedView)
-			{
-				case "ChatsView" when item != null:
-				{
-					foreach (var folder in _folders)
-					{
-						if (item.Name == $"{folder.Title}_{folder.Id}")
-						{
-							App._folderId = folder.Id;
-							break;
-						}
-						else if (item.Name == "NavViewChats")
-						{
-							App._folderId = -1;
-							break;
-						}
-						else
-						{
-							continue;
-						}
-					}
-
-					break;
-				}
-				case "NewUpdate":
-					ContentDialogNewVersion.ShowAsync();
-					return false;
-				case "InstallUpdate":
-					_updateManager.ApplyUpdate(_updateFilePath);
-					return false;
-			}
-			
-			var view = Assembly.GetExecutingAssembly().GetType($"{Config.AppName}.Views.{clickedView}");
-
-			if (string.IsNullOrEmpty(clickedView) || view == null)
-				return false;
-
-			ContentFrame.Navigate(view, null, new EntranceNavigationTransitionInfo());
-
-			NavigationView.PaneDisplayMode = clickedView switch
-			{
-				"SettingsView" or "EditFolders" => NavigationViewPaneDisplayMode.LeftCompact,
-				"ChatsView" => NavigationViewPaneDisplayMode.Left,
-				_ => NavigationView.PaneDisplayMode
-			};
-
-			return true;
-        }
-
-        private void NavView_Loaded(object sender, RoutedEventArgs e)
-		{
-			
-		}
-
-		private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
-		{
-
-		}
-
 		private async void ContentDialogNewVersion_OnPrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
 		{
-			NavigationView.FooterMenuItems.RemoveAt(NavigationView.FooterMenuItems.Count - 1);
+			//NavigationView.FooterMenuItems.RemoveAt(NavigationView.FooterMenuItems.Count - 1);
 			
 			_updateFilePath = await _updateManager.UpdateApplicationAsync();
 			
@@ -269,7 +154,7 @@ namespace sakuragram
 			item.Tag = "InstallUpdate";
 			item.Icon = new SymbolIcon(Symbol.Refresh);
 			
-			NavigationView.FooterMenuItems.Add(item);
+			//NavigationView.FooterMenuItems.Add(item);
 		}
 
 		private void ContentDialogNewVersion_OnCloseButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
@@ -281,7 +166,7 @@ namespace sakuragram
 			item.Tag = "NewUpdate";
 			item.Icon = new SymbolIcon(Symbol.Refresh);
 			
-			NavigationView.FooterMenuItems.Add(item);
+			//NavigationView.FooterMenuItems.Add(item);
 			
 			_updateAvailable = true;
 		}
