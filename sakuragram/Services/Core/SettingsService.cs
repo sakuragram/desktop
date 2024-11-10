a078101abc664c1e51aca988110903a5fc8cb85f
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-11-10 16:05:00 +0300

    Create SettingsService.cs

---
+++ sakuragram/Services/Core/SettingsService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace sakuragram.Services.Core;
+
+public class SettingsService
+{
+    private static readonly string SettingsPath = Path.Combine(AppContext.BaseDirectory, "sakura_settings.json");
+    
+    public static void SaveSettings(Settings settings)
+    {
+        string jsonSettings = JsonConvert.SerializeObject(settings);
+        File.WriteAllText(SettingsPath, jsonSettings);
+    }
+    
+    public static Settings LoadSettings()
+    {
+        if (!File.Exists(SettingsPath)) return new Settings { AutoUpdate = true, InstallBeta = false };
+        string jsonSettings = File.ReadAllText(SettingsPath);
+        return JsonConvert.DeserializeObject<Settings>(jsonSettings);
+    }
+}
+
+public class Settings
+{
+    public bool InstallBeta { get; set; }
+    public bool AutoUpdate { get; set; }
+    public string Language { get; set; }
+    public string ChatBottomFastAction { get; set; }
+}
