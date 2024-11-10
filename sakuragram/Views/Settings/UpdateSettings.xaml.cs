e645c12d81976e309f8e85a897b49260d71fac22
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-11-10 16:08:47 +0300

    now the update settings are working correctly

--- sakuragram/Views/Settings/UpdateSettings.xaml.cs
+++ sakuragram/Views/Settings/UpdateSettings.xaml.cs
@@ -2,12 +2,13 @@
 using CommunityToolkit.WinUI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using sakuragram.Services.Core;
 
 namespace sakuragram.Views.Settings;
 
 public partial class UpdateSettings : Page
 {
-    private static ApplicationDataContainer _localSettings;
+    private static Services.Core.Settings _localSettings;
     private string _appName;
     private readonly string _appLatestVersion;
     private string _appLatestVersionLink;
@@ -18,43 +19,10 @@ public partial class UpdateSettings : Page
 
         DispatcherQueue.EnqueueAsync(() =>
         {
-            if (ApplicationData.Current.LocalSettings.Containers.ContainsKey(Config.AppName))
-            {
-                _localSettings = ApplicationData.Current.LocalSettings.Containers[Config.AppName];
-            }
-            else
-            {
-                _localSettings = ApplicationData.Current.LocalSettings.CreateContainer(Config.AppName, ApplicationDataCreateDisposition.Always);
-            }
+            _localSettings = SettingsService.LoadSettings();
             
-            #region Settings
-
-            if (_localSettings != null)
-            {
-                if (_localSettings.Values["AutoUpdate"] == null)
-                {
-                    ToggleSwitchAutoUpdate.IsOn = true;
-                    _localSettings.Values["AutoUpdate"] = true;
-                }
-                else
-                {
-                    var autoUpdateValue = _localSettings.Values["AutoUpdate"];
-                    ToggleSwitchAutoUpdate.IsOn = (bool)autoUpdateValue;
-                }
-            
-                if (_localSettings.Values["InstallBeta"] == null)
-                {
-                    ToggleSwitchInstallBeta.IsOn = false;
-                    _localSettings.Values["InstallBeta"] = false;
-                }
-                else
-                {
-                    var installBetaValue = _localSettings.Values["InstallBeta"];
-                    ToggleSwitchInstallBeta.IsOn = (bool)installBetaValue;
-                }
-            }
-
-            #endregion
+            ToggleSwitchInstallBeta.IsOn = _localSettings.InstallBeta;
+            ToggleSwitchAutoUpdate.IsOn = _localSettings.AutoUpdate;
         });
     }
     
@@ -62,12 +30,20 @@ public partial class UpdateSettings : Page
 
     private void ToggleSwitch_OnToggled(object sender, RoutedEventArgs e)
     {
-        _localSettings.Values["InstallBeta"] = ToggleSwitchInstallBeta.IsOn;
+        SettingsService.SaveSettings(new Services.Core.Settings
+        {
+            InstallBeta = ToggleSwitchInstallBeta.IsOn,
+            AutoUpdate = ToggleSwitchAutoUpdate.IsOn
+        });
     }
 
     private void ToggleSwitchAutoUpdate_OnToggled(object sender, RoutedEventArgs e)
     {
-        _localSettings.Values["AutoUpdate"] = ToggleSwitchAutoUpdate.IsOn;
+        SettingsService.SaveSettings(new Services.Core.Settings
+        {
+            InstallBeta = ToggleSwitchInstallBeta.IsOn,
+            AutoUpdate = ToggleSwitchAutoUpdate.IsOn
+        });
     }
 
     #endregion
