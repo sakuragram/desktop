5b9b7d8dd959997e4cbb6375fc19a3aba1cefaea
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-11-10 13:37:27 +0300

    Create MediaPlayer.cs

---
+++ sakuragram/Controls/Messages/MediaPlayer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace sakuragram.Controls.Messages;
+
+public class MediaPlayer : Window
+{
+    public MediaPlayer()
+    {
+        Title = "MediaPlayer";
+    }
+}
