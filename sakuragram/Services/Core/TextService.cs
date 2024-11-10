efcae9cba3ea40afa46525947a80a30c5ff40be8
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-11-11 00:31:27 +0300

    Create TextService.cs

---
+++ sakuragram/Services/Core/TextService.cs
@@ -0,0 +1,10 @@
+﻿namespace sakuragram.Services.Core;
+
+public class TextService
+{
+    public static string Truncate(string value, int maxLength)
+    {
+        if (string.IsNullOrEmpty(value)) return value;
+        return value.Length <= maxLength ? value : value.Substring(0, maxLength) + "...";
+    }
+}
