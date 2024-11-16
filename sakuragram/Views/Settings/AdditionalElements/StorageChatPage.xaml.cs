321bf295aeac3cb05fd0d0cd6b2cc9c034f77bfa
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-11-17 00:56:47 +0300

    added storage page in to settings

---
+++ sakuragram/Views/Settings/AdditionalElements/StorageChatPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommunityToolkit.WinUI.Controls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using TdLib;
+
+namespace sakuragram.Views.Settings.AdditionalElements;
+
+public partial class StorageChatPage : Page
+{
+    private readonly TdClient _client = App._client;
+
+    public StorageChatPage()
+    {
+        InitializeComponent();
+        
+        ButtonReturn.Click += (_, _) => Frame.Navigate(typeof(Storage));
+    }
+
+    public async Task InitializePage(TdApi.StorageStatisticsByChat statisticsByChat)
+    {
+        try
+        {
+            var chat = await _client.GetChatAsync(statisticsByChat.ChatId);
+            TextBlockChatTitle.Text = chat.Title;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            TextBlockChatTitle.Text = "None";
+        }
+        
+        foreach (var statisticsByFileType in statisticsByChat.ByFileType)
+        {
+            SettingsCard settingsCard = new();
+            settingsCard.Header = statisticsByFileType.FileType switch
+            {
+                TdApi.FileType.FileTypeAnimation => "Animation",
+                TdApi.FileType.FileTypeAudio => "Audio",
+                TdApi.FileType.FileTypeDocument => "Document",
+                TdApi.FileType.FileTypeNone => "None",
+                TdApi.FileType.FileTypeNotificationSound => "Notification Sound",
+                TdApi.FileType.FileTypePhoto => "Photo",
+                TdApi.FileType.FileTypePhotoStory => "Photo Story",
+                TdApi.FileType.FileTypeProfilePhoto => "Profile Photo",
+                TdApi.FileType.FileTypeSecret => "Secret",
+                TdApi.FileType.FileTypeSecretThumbnail => "Secret Thumbnail",
+                TdApi.FileType.FileTypeSecure => "Secure",
+                TdApi.FileType.FileTypeSticker => "Sticker",
+                TdApi.FileType.FileTypeThumbnail => "Thumbnail",
+                TdApi.FileType.FileTypeUnknown => "Unknown",
+                TdApi.FileType.FileTypeVideo => "Video",
+                TdApi.FileType.FileTypeVideoNote => "Video Note",
+                TdApi.FileType.FileTypeVideoStory => "Video Story",
+                TdApi.FileType.FileTypeVoiceNote => "Voice Note",
+                TdApi.FileType.FileTypeWallpaper => "Wallpaper",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            settingsCard.Description = $"{statisticsByFileType.Count} files, {statisticsByFileType.Size / 1024 / 1024} MB";
+            PanelStorageFileTypes.Children.Add(settingsCard);
+        }
+    }
+}
---
+++ sakuragram/Views/Settings/Storage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommunityToolkit.WinUI;
+using CommunityToolkit.WinUI.Controls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using sakuragram.Views.Settings.AdditionalElements;
+using TdLib;
+
+namespace sakuragram.Views.Settings;
+
+public partial class Storage : Page
+{
+    private TdClient _client = App._client;
+    public Frame Frame { get; set; }
+    
+    public Storage()
+    {
+        InitializeComponent();
+        
+        Task.Run(async () =>
+        {
+            var storageUsageFast = await _client.GetStorageStatisticsFastAsync();
+            var storageUsage = await _client.GetStorageStatisticsAsync();
+            
+            await DispatcherQueue.EnqueueAsync(() =>
+            {
+                CardDatabaseSize.Description = $"Database size: {storageUsageFast.DatabaseSize / 1024 / 1024} MB";
+                CardLogSize.Description = $"Log size: {storageUsageFast.LogSize / 1024 / 1024} MB";
+                CardFilesSize.Description = $"Files size: {storageUsageFast.FilesSize / 1024 / 1024} MB ({storageUsageFast.FileCount} files)";
+                
+                ExpanderStorageByChat.Description = $"{storageUsage.ByChat.Length} chats";
+
+                foreach (var statisticsByChat in storageUsage.ByChat)
+                {
+                    SettingsCard card = new();
+                    card.Header = statisticsByChat.ChatId;
+                    card.Description = $"Database size: {statisticsByChat.Size / 1024 / 1024} MB";
+                    card.IsClickEnabled = true;
+                    card.Click += async (_, _) =>
+                    {
+                        Frame.Navigate(typeof(StorageChatPage));
+                        if (Frame.Content is StorageChatPage page) await page.InitializePage(statisticsByChat);
+                    };
+                    
+                    ExpanderStorageByChat.Items.Add(card);
+                }
+            });
+        });
+    }
+
+    private async void ButtonOptimizeStorage_OnClick(object sender, RoutedEventArgs e)
+    {
+        await _client.ExecuteAsync(new TdApi.OptimizeStorage());
+    }
+}
