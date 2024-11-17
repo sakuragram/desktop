ebe98e7ecc655047366f65581f05a2d5935d58c4
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-11-17 22:59:40 +0300

    Create Logger.cs

---
+++ sakuragram/Services/Core/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using static sakuragram.Services.Core.Logger.LogType;
+using Debug = System.Diagnostics.Debug;
+
+namespace sakuragram.Services.Core;
+
+public class Logger
+{
+    public enum LogType
+    {
+        Error,
+        Info,
+        Warning,
+        Debug,
+        Fatal
+    }
+    
+    public static void Log(LogType type, object value)
+    {
+        Console.ForegroundColor = type switch
+        {
+            Error => ConsoleColor.Red,
+            Info => ConsoleColor.Cyan,
+            Warning => ConsoleColor.Yellow,
+            LogType.Debug => ConsoleColor.Magenta,
+            Fatal => ConsoleColor.DarkRed,
+            _ => Console.ForegroundColor
+        };
+        
+        Debug.WriteLine(type + ": " + value);
+        Console.ResetColor();
+    }
+}
