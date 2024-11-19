79648a6e6d370bc6f8ad0888d55659fedb5575b2
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-11-20 02:05:42 +0300

    added privacy settings from current user

--- sakuragram/Views/Settings/PrivacyAndSecurity.xaml.cs
+++ sakuragram/Views/Settings/PrivacyAndSecurity.xaml.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using CommunityToolkit.WinUI;
+using CommunityToolkit.WinUI.Controls;
 using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
+using sakuragram.Controls.User;
 using sakuragram.Services;
 using sakuragram.Services.Core;
 using sakuragram.Views.Settings.AdditionalElements;
@@ -40,6 +44,95 @@ public partial class PrivacyAndSecurity : Page
         _canOpenConnectedWebsites = true;
         CardActiveSessions.Description = $"There are currently {activeSessions.Sessions_.Length} active sessions";
         _canOpenActiveSessions = true;
+        
+        await GetSettingAndUpdateUi(new TdApi.UserPrivacySetting.UserPrivacySettingShowStatus(),
+            null, TextBlockLastSeenAndOnline, ComboBoxLastSeen);
+        await GetSettingAndUpdateUi(new TdApi.UserPrivacySetting.UserPrivacySettingShowProfilePhoto(),
+            ExpanderProfilePhotos, TextBlockProfilePhotos, ComboBoxProfilePhotos);
+        await GetSettingAndUpdateUi(new TdApi.UserPrivacySetting.UserPrivacySettingShowBirthdate(),
+            ExpanderDateOfBirth, TextBlockDateOfBirth, ComboBoxDateOfBirth);
+        await GetSettingAndUpdateUi(new TdApi.UserPrivacySetting.UserPrivacySettingShowLinkInForwardedMessages(),
+            ExpanderForwardedMessages, TextBlockForwardedMessages, ComboBoxForwardedMessages);
+        await GetSettingAndUpdateUi(new TdApi.UserPrivacySetting.UserPrivacySettingAllowCalls(),
+            ExpanderCalls, TextBlockCalls, ComboBoxCalls);
+        await GetSettingAndUpdateUi(new TdApi.UserPrivacySetting.UserPrivacySettingAllowChatInvites(),
+            ExpanderGroupsAndChannels, TextBlockGroupsAndChannels, ComboBoxGroupsAndChannels);
+        await GetSettingAndUpdateUi(new TdApi.UserPrivacySetting.UserPrivacySettingAllowPrivateVoiceAndVideoNoteMessages(),
+            ExpanderVoiceMessages, TextBlockVoiceMessages, ComboBoxVoiceMessages);
+        await GetSettingAndUpdateUi(new TdApi.UserPrivacySetting.UserPrivacySettingShowPhoneNumber(),
+            ExpanderPhoneNumber, PhoneNumber, ComboBoxPhoneNumber);
+        await GetSettingAndUpdateUi(new TdApi.UserPrivacySetting.UserPrivacySettingAllowFindingByPhoneNumber(),
+            null, null, ComboBoxFindMeByNumber);
+    }
+    
+    private async Task GetSettingAndUpdateUi(TdApi.UserPrivacySetting setting, SettingsExpander expander, 
+        TextBlock textBlock, ComboBox comboBox)
+    {
+        var userPrivacySetting = await _client.GetUserPrivacySettingRulesAsync(setting);
+        
+        if (userPrivacySetting.Rules.Length > 0)
+        {
+            string rule = string.Empty;
+            int restricts = 0;
+            
+            foreach (var userPrivacySettingRule in userPrivacySetting.Rules)
+            {
+                switch (userPrivacySettingRule)
+                {
+                    case TdApi.UserPrivacySettingRule.UserPrivacySettingRuleAllowAll:
+                        rule = "Everybody";
+                        break;
+                    case TdApi.UserPrivacySettingRule.UserPrivacySettingRuleAllowContacts:
+                        rule = "My contacts";
+                        break;
+                    case TdApi.UserPrivacySettingRule.UserPrivacySettingRuleRestrictAll:
+                        rule = "Nobody";
+                        break;
+                    case TdApi.UserPrivacySettingRule.UserPrivacySettingRuleRestrictChatMembers restrictChatMembers:
+                        restricts += restrictChatMembers.ChatIds.Length;
+                        break;
+                    case TdApi.UserPrivacySettingRule.UserPrivacySettingRuleRestrictUsers restrictUsers:
+                        restricts += restrictUsers.UserIds.Length;
+                        break;
+                }
+                
+                if (comboBox != null) comboBox.SelectedIndex = userPrivacySettingRule switch
+                {
+                    TdApi.UserPrivacySettingRule.UserPrivacySettingRuleAllowAll => 0,
+                    TdApi.UserPrivacySettingRule.UserPrivacySettingRuleAllowContacts => 1,
+                    TdApi.UserPrivacySettingRule.UserPrivacySettingRuleRestrictAll => 2,
+                    _ => 0
+                };
+            }
+            
+            if (textBlock != null) textBlock.Text = restricts > 0 ? $"{rule} (-{restricts})" : rule;
+            if (comboBox != null) comboBox.SelectionChanged += (_, _) =>
+            {
+                comboBox.SelectionChanged += async (_, _) =>
+                {
+                    TdApi.UserPrivacySettingRule rule;
+                    switch (comboBox.SelectedIndex)
+                    {
+                        case 0:
+                            rule = new TdApi.UserPrivacySettingRule.UserPrivacySettingRuleAllowAll();
+                            break;
+                        case 1:
+                            rule = new TdApi.UserPrivacySettingRule.UserPrivacySettingRuleAllowContacts();
+                            break;
+                        case 2:
+                            rule = new TdApi.UserPrivacySettingRule.UserPrivacySettingRuleRestrictAll();
+                            break;
+                        default:
+                            rule = new TdApi.UserPrivacySettingRule.UserPrivacySettingRuleAllowAll();
+                            break;
+                    }
+                    await _client.SetUserPrivacySettingRulesAsync(setting, new TdApi.UserPrivacySettingRules
+                    {
+                        Rules = [rule]
+                    });
+                };
+            };
+        }
     }
     
     private void DeleteIfAway_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -128,13 +221,13 @@ public partial class PrivacyAndSecurity : Page
         textStackPanel.Orientation = Orientation.Vertical;
         textStackPanel.HorizontalAlignment = HorizontalAlignment.Stretch;
         textStackPanel.VerticalAlignment = VerticalAlignment.Stretch;
-        
-        PersonPicture botPhoto  = new();
+
+        ProfilePhoto botPhoto = new();
+        await botPhoto.InitializeProfilePhoto(bot, null);
         botPhoto.Margin = new Thickness(10);
         botPhoto.Width = 42;
         botPhoto.Height = 42;
         mainStackPanel.Children.Add(botPhoto);
-        await MediaService.GetUserPhoto(bot, botPhoto);
         
         TextBlock botNameTextBlock = new();
         botNameTextBlock.FontSize = 14;
@@ -192,5 +285,46 @@ public partial class PrivacyAndSecurity : Page
 
     private void ButtonBlockedUsers_OnClick(object sender, RoutedEventArgs e)
     {
+    }
+
+    private void ComboBoxPhoneNumber_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        TdApi.UserPrivacySettingRule rule = ComboBoxPhoneNumber.SelectedIndex switch
+        {
+            0 => new TdApi.UserPrivacySettingRule.UserPrivacySettingRuleAllowAll(),
+            1 => new TdApi.UserPrivacySettingRule.UserPrivacySettingRuleAllowContacts(),
+            2 => new TdApi.UserPrivacySettingRule.UserPrivacySettingRuleRestrictAll(),
+            _ => null
+        };
+
+        CardFindMeByNumber.Visibility = ComboBoxPhoneNumber.SelectedIndex switch
+        {
+            0 => Visibility.Collapsed,
+            1 => Visibility.Collapsed,
+            2 => Visibility.Visible,
+            _ => Visibility.Collapsed
+        };
+        
+        _client.ExecuteAsync(new TdApi.SetUserPrivacySettingRules
+        {
+            Rules = new TdApi.UserPrivacySettingRules { Rules = [rule] },
+            Setting = new TdApi.UserPrivacySetting.UserPrivacySettingShowPhoneNumber()
+        });
+    }
+
+    private void ComboBoxFindMeByNumber_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        TdApi.UserPrivacySettingRule rule = ComboBoxFindMeByNumber.SelectedIndex switch
+        {
+            0 => new TdApi.UserPrivacySettingRule.UserPrivacySettingRuleAllowAll(),
+            1 => new TdApi.UserPrivacySettingRule.UserPrivacySettingRuleAllowContacts(),
+            _ => null
+        };
+
+        _client.ExecuteAsync(new TdApi.SetUserPrivacySettingRules
+        {
+            Rules = new TdApi.UserPrivacySettingRules { Rules = [rule] },
+            Setting = new TdApi.UserPrivacySetting.UserPrivacySettingAllowFindingByPhoneNumber(),
+        });
     }
 }
