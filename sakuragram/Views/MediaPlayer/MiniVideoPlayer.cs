ff7d8b1ebf37e6a89ae9b6cd49358ca98793b996
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-11-22 01:39:58 +0300

    added mini video player for messages

---
+++ sakuragram/Views/MediaPlayer/MiniVideoPlayer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml.Input;
+
+namespace sakuragram.Views.MediaPlayer;
+
+public partial class MiniVideoPlayer
+{
+    private void MiniVideoPlayer_OnPointerEntered(object sender, PointerRoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void MiniVideoPlayer_OnPointerExited(object sender, PointerRoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
